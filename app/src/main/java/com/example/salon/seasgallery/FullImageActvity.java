6e51936a50ac32a18fa0d686b7f724184da013c6
RuidosoBSD <RuidosoBSD@gmail.com>
2017-07-12 14:47:08 +0200

    Ejercicio FeedBack Galeria
    Ejercicio FeedBack Galeria

---
+++ app/src/main/java/com/example/salon/seasgallery/FullImageActvity.java
@@ -0,0 +1,21 @@
+package com.example.salon.seasgallery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullImageActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image_actvity);
+        Intent i = getIntent();
+        int position = i.getExtras().getInt("id");
+        ImageAdapter adapter = new ImageAdapter(this);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(adapter.images[position]);
+
+    }
+}
